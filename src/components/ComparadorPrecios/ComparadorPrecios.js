dd947e2b4f45b10626a16d2fff00bbc38587ab09
V.Raskovsky <143296661+TuMaravilla@users.noreply.github.com>
2023-11-19 20:15:55 -0300

    arreglos y boton para borrar las listas

--- src/components/ComparadorPrecios/ComparadorPrecios.js
+++ src/components/ComparadorPrecios/ComparadorPrecios.js
@@ -1,81 +1,95 @@
 import React, { useState } from 'react';
+import './Comparador.css';
 
 function ComparadorDePrecios (){
+ 
+    const [ListaProductos, setListaProducto] = useState([]);
+    const [productoNombre, setProductoNombre] = useState('');
+    const [productoPrecio, setProductoPrecio] = useState('');
+    const [productoDistribuidor, setProductoDistribuidor] = useState('');
 
-      const ComparadorPrecios = () => {
-        const [ListaProductos, setListaProducto] = useState([]);
-        const [productoNombre, setProductoNombre] = useState('');
-        const [productoPrecio, setProductoPrecio] = useState('');
-        const [productoDistribuidor, setProductoDistribuidor] = useState('');
+      const AñadirProducto = () => {
+        const newProducto = {
+          nombre: productoNombre,
+          precio: parseFloat(productoPrecio),
+          distribuidor: productoDistribuidor,
+        };
 
-        const AñadirProducto = () => {
-          const newProducto = {
-            nombre: productoNombre,
-            precio: parseFloat(productoPrecio),
-            distribuidor: productoDistribuidor,
-          };
+      setListaProducto([...ListaProductos, newProducto]);
 
-          setListaProducto([...ListaProductos, newProducto]);
-
-          // Limpia los campos después de agregar el producto
-          setProductoNombre('');
+          setProductoNombre(''); // limpia los campos despues d agregar un producto
           setProductoPrecio('');
           setProductoDistribuidor('');
         };
 
-        const MinimoPrecio = (distribuidor) => {
+      const MinimoPrecio = (distribuidor) => {
           const productosDistribuidor = ListaProductos.filter(producto => producto.distribuidor === distribuidor);
           const productoPrecioMin = productosDistribuidor.reduce((min, producto) => (min.precio < producto.precio ? min : producto), {});
 
           return productoPrecioMin;
         };
 
-        return (
-          <div>
-            <h2>Comparador de Precios</h2>
+      const borrarListas = () => {  // limpia ambas listas
+          setListaProducto([]);
+          setProductoDistribuidor([]);
+        };    
 
-            <div>
-              <h3>Agregar Producto</h3>
-              <div>
-                <label>Producto:</label>
-                <input type="text" value={productoNombre} onChange={(e) => setProductoNombre(e.target.value)} />
-              </div>
-              <div>
-                <label>Precio:</label>
-                <input type="number" value={productoPrecio} onChange={(e) => setProductoPrecio(e.target.value)} />
-              </div>
-              <div>
-                <label>Distribuidor:</label>
-                <input type="text" value={productoDistribuidor} onChange={(e) => setProductoDistribuidor(e.target.value)} />
-              </div>
-              <button onClick={AñadirProducto}>Agregar a la Lista</button>
-            </div>
+  return (
+    <div>
+      <h1 className="titulo">Comparador de Precios</h1>
 
-            <div>
-              <h3>Lista de Productos</h3>
-              <ul>
-                {ListaProductos.map((producto, index) => (
-                  <li key={index}>
-                    {producto.nombre} - {producto.distribuidor}: ${producto.precio}
-                  </li>
-                ))}
-              </ul>
-            </div>
+        <div>
+          <h3 className="tituloCarga">Agregue sus productos a la lista</h3>
 
-            <div>
-              <h3>Menor precio por Distribuidor</h3>
-              <ul>
-                {[...new Set(ListaProductos.map(producto => producto.distribuidor))].map((distribuidor, index) => (
-                  <li key={index}>
-                    {distribuidor}: {MinimoPrecio(distribuidor).nombre} - ${MinimoPrecio(distribuidor).precio}
-                  </li>
-                ))}
-              </ul>
-            </div>
+          <div className="contenedores">
+            <label>Producto:</label>
+            <input type="text" value={productoNombre} onChange={(e) => setProductoNombre(e.target.value)} />
           </div>
-        );
-      };
 
-}
+          <div className="contenedores">
+            <label>Precio:</label>
+            <input type="number" value={productoPrecio} onChange={(e) => setProductoPrecio(e.target.value)} />
+          </div>
+          
+          <div className="contenedores">
+            <label>Distribuidor:</label>
+            <input type="text" value={productoDistribuidor} onChange={(e) => setProductoDistribuidor(e.target.value)} />
+          </div>
+          
+          <button onClick={AñadirProducto} className="botones">Agregar a la lista</button>
+        </div>
+
+        <div className="listas"> 
+          <div className="listaProducto" >
+            <h3>Lista de los productos</h3>
+            <ul>
+              {ListaProductos.map((producto, index) => (
+                <li key={index}>
+                  {producto.nombre} - {producto.distribuidor}: ${producto.precio}
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="listaMinPrecio">
+            <h3>Menor precio por el distribuidor</h3>
+           
+            <ul>
+              {[...new Set(ListaProductos.map(producto => producto.distribuidor))].map((distribuidor, index) => (
+                <li key={index}>
+                  {distribuidor}: {MinimoPrecio(distribuidor).nombre} - ${MinimoPrecio(distribuidor).precio}
+                </li>
+              ))}
+           
+            </ul>
+          </div>
+        </div>
+
+        {/* boton para borrar las listas */}
+      <button onClick={borrarListas} className="botones">Borrar las listas</button>          
+
+    </div>        
+  );
+};
 
 export default ComparadorDePrecios;
